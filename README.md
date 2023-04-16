2bf159d4a6de12e81facb2e6c6c99a5c3a088397
jidicula <git@johanan.dev>
2023-04-16 16:19:16 -0400

    Create Azure Function

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-[![Build](https://github.com/jidicula/template-go/actions/workflows/build.yml/badge.svg)](https://github.com/jidicula/template-go/actions/workflows/build.yml) [![Latest Release](https://github.com/jidicula/template-go/actions/workflows/release-draft.yml/badge.svg)](https://github.com/jidicula/template-go/actions/workflows/release-draft.yml) [![Go Report Card](https://goreportcard.com/badge/github.com/jidicula/template-go)](https://goreportcard.com/report/github.com/jidicula/template-go) [![Go Reference](https://pkg.go.dev/badge/github.com/jidicula/template-go.svg)](https://pkg.go.dev/github.com/jidicula/template-go)
+[![Build](https://github.com/jidicula/cloudflare-cachebuster/actions/workflows/build.yml/badge.svg)](https://github.com/jidicula/cloudflare-cachebuster/actions/workflows/build.yml) [![Latest Release](https://github.com/jidicula/cloudflare-cachebuster/actions/workflows/release-draft.yml/badge.svg)](https://github.com/jidicula/cloudflare-cachebuster/actions/workflows/release-draft.yml) [![Go Report Card](https://goreportcard.com/badge/github.com/jidicula/cloudflare-cachebuster)](https://goreportcard.com/report/github.com/jidicula/cloudflare-cachebuster) [![Go Reference](https://pkg.go.dev/badge/github.com/jidicula/cloudflare-cachebuster.svg)](https://pkg.go.dev/github.com/jidicula/cloudflare-cachebuster)
 
-# template-go
+# cloudflare-cachebuster
+
+Azure Function that calls Cloudflares [`purge_cache`](https://developers.cloudflare.com/api/operations/zone-purge) endpoint.
+
+Requires 2 environment variables to be set:
+* `CLOUDFLARE_ZONEID`: the Zone ID of the cache being purged.
+* `CLOUDFLARE_PAT`: A Cloudflare PAT with `Zone.Cache Purge` permissions for the cache's zone.
+
+## Deploying to Azure via GitHub Actions
+Requires:
+* `AZURE_RBAC_CREDENTIALS`: secret containing a [service principal's RBAC credentials](https://github.com/marketplace/actions/azure-functions-action#using-azure-service-principal-for-rbac-as-deployment-credential).
+* `AZURE_FUNCTIONAPP_NAME`: variable containing the Function App's name.
--- main.go
+++ main.go
@@ -1,7 +1,86 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+var zoneID = os.Getenv("CLOUDFLARE_ZONEID")
+var pat = os.Getenv("CLOUDFLARE_PAT")
 
 func main() {
-	fmt.Println("Hello, world!")
+	customHandlerPort, exists := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT")
+	if !exists {
+		customHandlerPort = "8080"
+	}
+	http.HandleFunc("/api/CloudflareCachebuster", purgeCacheHandler)
+	log.Printf("Listening on %s. Serving at http://127.0.0.1:%s/", customHandlerPort, customHandlerPort)
+
+	log.Fatal(http.ListenAndServe(":"+customHandlerPort, nil))
+}
+
+// httpError logs the error and returns an HTTP error message and code.
+func httpError(w http.ResponseWriter, err error, msg string, errorCode int) {
+	errorMsg := fmt.Sprintf("%s: %v", msg, err)
+	log.Printf("%s", errorMsg)
+	http.Error(w, errorMsg, errorCode)
+}
+
+func purgeCacheHandler(w http.ResponseWriter, r *http.Request) {
+
+	log.Printf("Received %s from %v", r.Method, r.RemoteAddr)
+	if r.Method == "POST" {
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			httpError(w, err, "error reading POST body", http.StatusInternalServerError)
+			return
+		}
+		log.Printf("Request body: %s", body)
+	}
+	// Send POST request to Cloudflare
+	client := &http.Client{}
+	endpoint := fmt.Sprintf("https://api.cloudflare.com/client/v4/zones/%s/purge_cache", zoneID)
+	data := `{"purge_everything":true}`
+	req, err := http.NewRequest("POST", endpoint, strings.NewReader(data))
+	if err != nil {
+		httpError(w, err, "error creating new Request", http.StatusInternalServerError)
+		return
+	}
+
+	authHeader := fmt.Sprintf("Bearer %s", pat)
+	req.Header.Add("Authorization", authHeader)
+	req.Header.Add("Content-Type", "application/json")
+
+	cloudflareResp, err := client.Do(req)
+	if err != nil {
+		httpError(w, err, "error sending POST request", http.StatusInternalServerError)
+		return
+	}
+	defer cloudflareResp.Body.Close()
+
+	// Pass cloudflare response to caller
+
+	cloudflareRespBody, err := io.ReadAll(cloudflareResp.Body)
+	if err != nil {
+		httpError(w, err, "error reading Cloudflare response", http.StatusInternalServerError)
+		return
+	}
+
+	if cloudflareResp.StatusCode != http.StatusOK {
+		msg := fmt.Sprintf("error non-200 status: %s", cloudflareRespBody)
+		httpError(w, nil, msg, http.StatusInternalServerError)
+		return
+	}
+
+	log.Printf("Cloudflare response: %s", cloudflareRespBody)
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(cloudflareRespBody)
+	if err != nil {
+		httpError(w, err, "error sending response to client", http.StatusInternalServerError)
+		return
+	}
 }
